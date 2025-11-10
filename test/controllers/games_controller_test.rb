51e1338eafad1d6cecf854253861b261075b55bb
superchilled <karl@superchilled.co.uk>
2025-11-10 14:14:15 +0000

    Fixing tests

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -2,12 +2,12 @@ require "test_helper"
 
 class GamesControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
-    get game_url(locale: :fr, id: "TestPlayer")
+    get player_game_url(locale: :fr, id: 1, player_id: 1)
     assert_response :success
   end
 
   test "should get new" do
-    get new_game_url(locale: :fr)
+    get new_player_game_url(locale: :fr, player_id: 1)
     assert_response :success
   end
 end
